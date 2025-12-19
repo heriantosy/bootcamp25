c2702d0778b4f7758c257303e7e8c4e2559bc711
ivnrzky18 <ivanredmi08@gmal.com>
2025-12-19 15:43:54 +0700

    Update app.blade.php

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -17,7 +17,7 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-white shadow-sm">
         <div class="container">
             <a class="navbar-brand fw-bold text-primary" href="#">
-                HTPBOOTCAMP
+                HTPBOOTCAMP25
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav">
                 <span class="navbar-toggler-icon"></span>
